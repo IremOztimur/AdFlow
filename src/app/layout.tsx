75de56ce2f3cface81cbdb469ad0bbce0f70265a
IremOztimur <iremoztimur07@gmail.com>
2026-01-09 14:17:44 +0300

    Update favicon: use adflow.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <link rel="icon" href="/adflow.svg" type="image/svg+xml" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
